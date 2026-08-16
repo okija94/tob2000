7a627552c96a356a0d56459df584292b30149177
Toby Obi-Uchendu <tobechukwuobiuchendu@yahoo.com>
2026-08-16 21:40:25 +1000

    Added storage account

--- variables.tf
+++ variables.tf
@@ -72,3 +72,15 @@ variable "client_secret" {
   type      = string
   sensitive = true
 }
+
+variable "storage_accounts" {
+  type = map(object({
+    location = string
+    resource_group_name= string
+    account_tier = string
+   account_replication_type= string
+    account_kind = string
+    is_hns_enabled = bool
+  }))
+  
+}
